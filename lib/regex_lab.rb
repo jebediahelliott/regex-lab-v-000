eae3122e324fe7e918d4ad6f00e690f9a2de7520
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-21 00:48:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,11 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.each do |number|
-    if number.match(/\d{10}/) == nil
-      return false
-    else
-      return true
-    end
-  end
+  phone.match(/.*\d{3}.*{3}.*{4}/)
 end
